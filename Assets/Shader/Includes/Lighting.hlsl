feda72cbc1a540967e538e4620a22b8879acfe24
Gagloger <140218770+Gagloger@users.noreply.github.com>
2024-09-30 19:40:39 -0500

    ToonLit, primera clase

--- Assets/Shader/Includes/Lighting.hlsl
+++ Assets/Shader/Includes/Lighting.hlsl
@@ -18,6 +18,25 @@ void GetMainLightInfo_float(out float3 direction, out half3 color)
     #endif
 }
 
+void ComputeAdditionalLightingToon_float(
+    float3 normalWS,float3 positionWS, UnityTexture2D toonRamp, UnitySamplerState sState,
+    out float3 diffuse){
+    diffuse=0;
+
+    #if !defined(SHADERGRAPH_PREVIEW)
+    int lightCount = GetAdditionalLightsCount();
+
+    [unroll(8)]
+    for (int lightId =0; lightId < lightCount; lightId++){
+        Light light = GetAdditionalLight(lightId,positionWS);
+        half halfLambert = dot(normalWS,light.direction)*0.5+0.5;
+        half4 toonDiffuse = SAMPLE_TEXTURE2D(toonRamp, sState, halfLambert);
+
+        diffuse += toonDiffuse * light.color * light.distanceAttenuation;
+    }
+    #endif
+}
+
 void Add_float(float a, float b, out float c){
 	c = a+b;
 }
